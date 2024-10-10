c0c909913751d6ec5c827aeb02f5cf19493cd6fa
Alvaro Lamoru Lopez <alvarolamoru@gmail.com>
2024-10-09 20:36:25 -0400

    terminando el estilo de las las letras bienvenidos

--- themes/eve/pages/homepage/MainBanner.jsx
+++ themes/eve/pages/homepage/MainBanner.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import "./MainBanner.scss";
-
 function MainBanner() {
   return (
     <div className="main-banner-home flex items-center">
-      <div className="page-width grid grid-cols-1 md:grid-cols-2 gap-2">
-        <div className="text-center md:text-left px-2 ">
+      <div className="page-width grid grid-cols-1 md:grid-cols-2 gap-2 pageWidth">
+        <div className="text-center md:text-left px-2 divContainer">
           <h2 className="h1">Bienvenidos</h2>
-          <p className="slogan">Tejiendo tus sueños con alegria y muchos colores </p>
-          <p></p>
-           <a className="button button-primary botonShowNow" href="#">
+          <p className="slogan">Tejiendo tus sueños con alegría y muchos colores</p>
+          <a className="button button-primary botonShowNow" href="#">
             SHOW NOW
-           </a>
-         </div>
-        <div />
+          </a>
+        </div>
       </div>
     </div>
   );
